489184e27b6216644d07ee1bd2a497a5508b1f00
Brad <powelbra@oregonstate.edu>
2019-07-22 16:46:06 -0700

    Update randomtestcard1.c

--- projects/powelbra/dominion/randomtestcard1.c
+++ projects/powelbra/dominion/randomtestcard1.c
@@ -24,7 +24,7 @@ int main() {
 
 	srand(time(0));
 	struct gameState pre, post;
-	int i, j, k, preBonus, postBonus, handPos, choice, isEstate;
+	int i, j, preBonus, postBonus, handPos, choice, isEstate;
 
 	for (i = 0; i < 2000; i++) {
 		// Generate a random game state with bytes from 0-255
@@ -40,6 +40,9 @@ int main() {
 		post.discardCount[post.whoseTurn] = (rand() % MAX_DECK) - 1;
 		post.handCount[post.whoseTurn] = (rand() % MAX_HAND) + 1;	// Handcount must be at least 1 to be the "baron" (handPos for discardCard in Baron)
 		post.playedCardCount = (rand() % MAX_HAND) - 1;
+		for (j = 0; j < post.handCount[post.whoseTurn]; j++) {
+			post.hand[post.whoseTurn][j] = rand() % 27;		// Randomize card in hand to valid kingdom cards
+		}
 
 		// Copy post to pre
 		memcpy(&pre, &post, sizeof(struct gameState));
@@ -59,9 +62,9 @@ int main() {
 		// Other post conditions of Baron depends on choice and if there's an estate
 		// Store that number for use in hand shifting later
 		isEstate = 0;
-		for (k = 0; k < pre.handCount[pre.whoseTurn]; k++) {
-			if (pre.hand[pre.whoseTurn][k] == estate) {
-				isEstate = k;
+		for (j = 0; j < pre.handCount[pre.whoseTurn]; j++) {
+			if (pre.hand[pre.whoseTurn][j] == estate) {
+				isEstate = j;
 				break;
 			}
 		}
@@ -74,8 +77,8 @@ int main() {
 				pre.discard[pre.whoseTurn][pre.discardCount[pre.whoseTurn]] = estate;
 				pre.discardCount[pre.whoseTurn]++;
 				// Shift cards in hand over
-				for (; k < pre.handCount[pre.whoseTurn]; k++) {
-					pre.hand[pre.whoseTurn][k] = pre.hand[pre.whoseTurn][k + 1];
+				for (; j < pre.handCount[pre.whoseTurn]; j++) {
+					pre.hand[pre.whoseTurn][j] = pre.hand[pre.whoseTurn][j + 1];
 				}
 				pre.hand[pre.whoseTurn][pre.handCount[pre.whoseTurn]] = -1;
 				pre.handCount[pre.whoseTurn]--;
